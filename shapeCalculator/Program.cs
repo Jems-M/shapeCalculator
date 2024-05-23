485bf04d6803b191e1c253028520b728f334c151
Jems-M <83780887+Jems-M@users.noreply.github.com>
2024-05-23 11:11:25 +0100

    Add project files.

---
+++ shapeCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace shapeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
